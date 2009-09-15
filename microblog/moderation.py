190bc87b335ff7edf6d56dfdd7fcc099f71ba519
David Mugnai <dvd@develer.com>
2009-09-15 11:16:57 +0200

    la presenza del modulo akismet è opzionale (in mancanza il supporto ad akismet viene disabilitato)

--- microblog/moderation.py
+++ microblog/moderation.py
@@ -6,7 +6,10 @@ from django.contrib.comments.moderation import CommentModerator, moderator
 from models import Post
 import settings
 
-import akismet
+try:
+    import akismet
+except ImportError:
+    akismet = None
 
 class PostModeration(CommentModerator):
     #email_notification = True
@@ -14,42 +17,43 @@ class PostModeration(CommentModerator):
     auto_moderate_field = 'date'
     moderate_after = 30
 
-    def moderate(self, comment, content_object, request):
-        r = super(PostModeration, self).moderate(comment, content_object, request)
-        if not r and settings.MICROBLOG_AKISMET_KEY:
-            aks = akismet.Akismet(
-                agent = 'Microblog',
-                key = settings.MICROBLOG_AKISMET_KEY, 
-                blog_url = dsettings.DEFAULT_URL_PREFIX + reverse('microblog-full-list')
-            )
-            try:
-                if aks.verify_key():
-                    m = request.META
-                    data = {
-                        'user_ip': m['REMOTE_ADDR'],
-                        'user_agent': m.get('HTTP_USER_AGENT', ''),
-                        'referrer': m.get('HTTP_REFERER', ''),
-                        'comment_type': 'comment',
-                        'comment_author': comment.user_name,
-                        'comment_author_email': comment.user_email,
-                        'comment_author_url': comment.user_url,
-                        'HTTP_ACCEPT': m.get('HTTP_ACCEPT', ''),
-                        'permalink': '',
-                        'SERVER_NAME': m.get('SERVER_NAME', ''),
-                        'SERVER_SOFTWARE': m.get('SERVER_SOFTWARE', ''),
-                        'SERVER_ADMIN': m.get('SERVER_ADMIN', ''),
-                        'SERVER_ADDR': m.get('SERVER_ADDR', ''),
-                        'SERVER_SIGNATURE': m.get('SERVER_SIGNATURE', ''),
-                        'SERVER_PORT': m.get('SERVER_PORT', ''),
-                    }
-                    r = aks.comment_check(comment.comment.encode('utf-8'), data, build_data = False)
-                elif dsettings.DEBUG:
-                    raise ValueError('Akismet: invalid key')
-            except:
-                if dsettings.DEBUG:
-                    raise
-                pass
-        return r
+    if akismet:
+        def moderate(self, comment, content_object, request):
+            r = super(PostModeration, self).moderate(comment, content_object, request)
+            if not r and settings.MICROBLOG_AKISMET_KEY:
+                aks = akismet.Akismet(
+                    agent = 'Microblog',
+                    key = settings.MICROBLOG_AKISMET_KEY, 
+                    blog_url = dsettings.DEFAULT_URL_PREFIX + reverse('microblog-full-list')
+                )
+                try:
+                    if aks.verify_key():
+                        m = request.META
+                        data = {
+                            'user_ip': m['REMOTE_ADDR'],
+                            'user_agent': m.get('HTTP_USER_AGENT', ''),
+                            'referrer': m.get('HTTP_REFERER', ''),
+                            'comment_type': 'comment',
+                            'comment_author': comment.user_name,
+                            'comment_author_email': comment.user_email,
+                            'comment_author_url': comment.user_url,
+                            'HTTP_ACCEPT': m.get('HTTP_ACCEPT', ''),
+                            'permalink': '',
+                            'SERVER_NAME': m.get('SERVER_NAME', ''),
+                            'SERVER_SOFTWARE': m.get('SERVER_SOFTWARE', ''),
+                            'SERVER_ADMIN': m.get('SERVER_ADMIN', ''),
+                            'SERVER_ADDR': m.get('SERVER_ADDR', ''),
+                            'SERVER_SIGNATURE': m.get('SERVER_SIGNATURE', ''),
+                            'SERVER_PORT': m.get('SERVER_PORT', ''),
+                        }
+                        r = aks.comment_check(comment.comment.encode('utf-8'), data, build_data = False)
+                    elif dsettings.DEBUG:
+                        raise ValueError('Akismet: invalid key')
+                except:
+                    if dsettings.DEBUG:
+                        raise
+                    pass
+            return r
 
 if settings.MICROBLOG_ENABLE_MODERATION:
     moderator.register(Post, PostModeration)
