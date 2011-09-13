a8b6cca9da21775ff1e61f63d4d09e3aaef95486
David Mugnai <dvd@develer.com>
2011-09-13 16:42:30 +0200

    test setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+import os
+import os.path
+
+def recurse(path):
+    B = 'microblog'
+    output = []
+    for dirpath, dirnames, filenames in os.walk(os.path.join(B, path)):
+        for f in filenames:
+            output.append(os.path.join(dirpath, f)[len(B)+1:])
+    return output
+
+setup(name='microblog',
+    version='0.1',
+    description='django microblog',
+    author='dvd',
+    author_email='dvd@develer.com',
+    packages=[
+        'microblog',
+        'microblog.management',
+        'microblog.templatetags',
+        'microblog.utils',
+    ],
+    package_data={
+        'microblog': sum(map(recurse, ('locale', 'static', 'templates')), []),
+    }
+)
