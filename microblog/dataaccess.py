918943427b9970a105811e099c90aaf97375c1c6
David Mugnai <dvd@gnx.it>
2012-01-06 15:48:00 +0100

    modulo per l'accesso e l'aggregazione dei dati con supporto alla cache

---
+++ microblog/dataaccess.py
@@ -0,0 +1,128 @@
+# -*- coding: UTF-8 -*-
+
+from django.core.cache import cache
+from django.db.models.signals import post_delete, post_save
+import functools
+import hashlib
+
+WEEK = 7 * 24 * 60 * 60 # 1 week
+def cache_me(key=None, ikey=None, signals=(), models=(), timeout=WEEK):
+    def hashme(k):
+        if isinstance(k, unicode):
+            k = k.encode('utf-8')
+        return hashlib.md5(k).hexdigest()
+    def decorator(f):
+
+        def invalidate(sender, **kwargs):
+            if ikey is None:
+                ks = (f.__name__,)
+            elif callable(ikey):
+                k = ikey(sender, **kwargs)
+                if isinstance(k, basestring):
+                    ks = (k,)
+                else:
+                    ks = k
+            else:
+                ks = (ikey,)
+            if ks:
+                print 'invalidate', ks
+                cache.delete_many(map(hashme, ks))
+
+        if ikey or (ikey is None and key is None):
+            for s in signals:
+                s.connect(invalidate, weak=False)
+
+            for m in models:
+                post_save.connect(invalidate, sender=m, weak=False)
+                post_delete.connect(invalidate, sender=m, weak=False)
+
+        def _key(*args, **kwargs):
+            if key is None:
+                k = f.__name__
+            elif callable(key):
+                k = key(*args, **kwargs)
+            else:
+                k = key % args
+            print 'k', k
+            return hashme(k)
+
+        @functools.wraps(f)
+        def wrapper(*args, **kwargs):
+            k = _key(*args, **kwargs)
+            data = cache.get(k)
+            if data is None:
+                print 'miss', k
+                data = f(*args, **kwargs)
+                cache.set(k, data, timeout)
+            else:
+                print 'hit', k
+            return data
+        wrapper.cachekey = _key
+            
+        return wrapper
+    return decorator
+
+from collections import defaultdict
+from django.conf import settings as dsettings
+from django.contrib import comments
+from django.core.urlresolvers import reverse
+from microblog import models
+from microblog import settings
+from taggit.models import TaggedItem
+
+def _i_post_list(sender, **kw):
+    ks = []
+    for l in dsettings.LANGUAGES:
+        ks.append('m:post_list:%s' % l[0])
+    return ks
+@cache_me(models=(models.Post,),
+    key='m:post_list:%s',
+    ikey=_i_post_list)
+def post_list(lang):
+    qs = models.Post.objects\
+            .byLanguage(lang)\
+            .select_related('category', 'author')
+    return list(qs)
+
+@cache_me(models=(models.Post,))
+def tag_map():
+    tmap = defaultdict(set)
+    items = TaggedItem.objects\
+        .filter(content_type__app_label='microblog', content_type__model='post')\
+        .select_related('tag')
+    for o in items:
+        tmap[o.object_id].add(o.tag)
+    return tmap
+
+def _i_post_data(sender, **kw):
+    if sender is models.Post:
+        pid = kw['instance'].id
+    else:
+        pid = kw['instance'].post_id
+    ks = []
+    for l in dsettings.LANGUAGES:
+        ks.append('m:post_data:%s%s' % (pid, l[0]))
+    return ks
+@cache_me(models=(models.Post, models.PostContent),
+    key='m:post_data:%s%s',
+    ikey=_i_post_data)
+def post_data(pid, lang):
+    post = models.Post.objects\
+        .select_related('author', 'category')\
+        .get(id=pid)
+    try:
+        content = post.content(lang=lang, fallback=True)
+    except models.PostContent.DoesNotExist:
+        content = None
+
+    comment_list = comments.get_model().objects\
+        .filter(content_type__app_label='microblog', content_type__model='post')\
+        .filter(object_pk=pid, is_public=True)
+
+    burl = models.PostContent.build_absolute_url(post, content)
+    return {
+        'post': post,
+        'content': content,
+        'url': reverse(burl[0], args=burl[1], kwargs=burl[2]),
+        'comments': list(comment_list),
+    }
