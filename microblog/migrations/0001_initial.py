d97983b5ef2ef1a7a90608316a19a9cdb0c66ac0
David Mugnai <dvd@gnx.it>
2014-11-01 19:27:09 +0100

    migrations, start over

--- microblog/migrations/0001_initial.py
+++ microblog/migrations/0001_initial.py
@@ -1,48 +1,59 @@
-# encoding: utf-8
-import datetime
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
 from south.db import db
 from south.v2 import SchemaMigration
 from django.db import models
 
+
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        
         # Adding model 'Category'
-        db.create_table('microblog_category', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+        db.create_table(u'microblog_category', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('name', self.gf('django.db.models.fields.CharField')(max_length=100)),
             ('description', self.gf('django.db.models.fields.TextField')(blank=True)),
         ))
-        db.send_create_signal('microblog', ['Category'])
+        db.send_create_signal(u'microblog', ['Category'])
 
         # Adding model 'Post'
-        db.create_table('microblog_post', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+        db.create_table(u'microblog_post', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('date', self.gf('django.db.models.fields.DateTimeField')(db_index=True)),
             ('author', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'])),
-            ('status', self.gf('django.db.models.fields.CharField')(default='P', max_length=1)),
-            ('allow_comments', self.gf('django.db.models.fields.BooleanField')(default=False, blank=True)),
-            ('tags', self.gf('tagging.fields.TagField')()),
+            ('status', self.gf('django.db.models.fields.CharField')(default='D', max_length=1)),
+            ('allow_comments', self.gf('django.db.models.fields.BooleanField')(default=False)),
             ('category', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['microblog.Category'])),
+            ('featured', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('image', self.gf('django.db.models.fields.URLField')(max_length=200, null=True, blank=True)),
         ))
-        db.send_create_signal('microblog', ['Post'])
+        db.send_create_signal(u'microblog', ['Post'])
+
+        # Adding model 'Spam'
+        db.create_table(u'microblog_spam', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('post', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['microblog.Post'])),
+            ('method', self.gf('django.db.models.fields.CharField')(max_length=1)),
+            ('value', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('date', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+        ))
+        db.send_create_signal(u'microblog', ['Spam'])
 
         # Adding model 'PostContent'
-        db.create_table('microblog_postcontent', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+        db.create_table(u'microblog_postcontent', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('post', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['microblog.Post'])),
             ('language', self.gf('django.db.models.fields.CharField')(max_length=3)),
             ('headline', self.gf('django.db.models.fields.CharField')(max_length=200)),
-            ('slug', self.gf('django.db.models.fields.SlugField')(max_length=50, db_index=True)),
+            ('slug', self.gf('django.db.models.fields.SlugField')(max_length=50)),
             ('summary', self.gf('django.db.models.fields.TextField')()),
             ('body', self.gf('django.db.models.fields.TextField')()),
         ))
-        db.send_create_signal('microblog', ['PostContent'])
+        db.send_create_signal(u'microblog', ['PostContent'])
 
         # Adding model 'Trackback'
-        db.create_table('microblog_trackback', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+        db.create_table(u'microblog_trackback', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('content', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['microblog.PostContent'])),
             ('type', self.gf('django.db.models.fields.CharField')(default='tb', max_length=2)),
             ('date', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
@@ -51,97 +62,121 @@ class Migration(SchemaMigration):
             ('title', self.gf('django.db.models.fields.TextField')()),
             ('excerpt', self.gf('django.db.models.fields.TextField')()),
         ))
-        db.send_create_signal('microblog', ['Trackback'])
+        db.send_create_signal(u'microblog', ['Trackback'])
 
 
     def backwards(self, orm):
-        
         # Deleting model 'Category'
-        db.delete_table('microblog_category')
+        db.delete_table(u'microblog_category')
 
         # Deleting model 'Post'
-        db.delete_table('microblog_post')
+        db.delete_table(u'microblog_post')
+
+        # Deleting model 'Spam'
+        db.delete_table(u'microblog_spam')
 
         # Deleting model 'PostContent'
-        db.delete_table('microblog_postcontent')
+        db.delete_table(u'microblog_postcontent')
 
         # Deleting model 'Trackback'
-        db.delete_table('microblog_trackback')
+        db.delete_table(u'microblog_trackback')
 
 
     models = {
-        'auth.group': {
+        u'auth.group': {
             'Meta': {'object_name': 'Group'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
-            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
         },
-        'auth.permission': {
-            'Meta': {'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
             'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
         },
-        'auth.user': {
+        u'auth.user': {
             'Meta': {'object_name': 'User'},
             'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
             'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
             'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True', 'blank': 'True'}),
-            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
-            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
             'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
             'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
-            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
             'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
         },
-        'contenttypes.contenttype': {
-            'Meta': {'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
             'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
         },
-        'microblog.category': {
+        u'microblog.category': {
             'Meta': {'object_name': 'Category'},
             'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
         },
-        'microblog.post': {
+        u'microblog.post': {
             'Meta': {'object_name': 'Post'},
-            'allow_comments': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
-            'author': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"}),
-            'category': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['microblog.Category']"}),
+            'allow_comments': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'author': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']"}),
+            'category': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['microblog.Category']"}),
             'date': ('django.db.models.fields.DateTimeField', [], {'db_index': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'status': ('django.db.models.fields.CharField', [], {'default': "'P'", 'max_length': '1'}),
-            'tags': ('tagging.fields.TagField', [], {})
+            'featured': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'status': ('django.db.models.fields.CharField', [], {'default': "'D'", 'max_length': '1'})
         },
-        'microblog.postcontent': {
+        u'microblog.postcontent': {
             'Meta': {'object_name': 'PostContent'},
             'body': ('django.db.models.fields.TextField', [], {}),
             'headline': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'language': ('django.db.models.fields.CharField', [], {'max_length': '3'}),
-            'post': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['microblog.Post']"}),
-            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50', 'db_index': 'True'}),
+            'post': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['microblog.Post']"}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50'}),
             'summary': ('django.db.models.fields.TextField', [], {})
         },
-        'microblog.trackback': {
-            'Meta': {'object_name': 'Trackback'},
+        u'microblog.spam': {
+            'Meta': {'object_name': 'Spam'},
+            'date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'method': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'post': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['microblog.Post']"}),
+            'value': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'microblog.trackback': {
+            'Meta': {'ordering': "['-date']", 'object_name': 'Trackback'},
             'blog_name': ('django.db.models.fields.TextField', [], {}),
-            'content': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['microblog.PostContent']"}),
+            'content': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['microblog.PostContent']"}),
             'date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
             'excerpt': ('django.db.models.fields.TextField', [], {}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'title': ('django.db.models.fields.TextField', [], {}),
             'type': ('django.db.models.fields.CharField', [], {'default': "'tb'", 'max_length': '2'}),
             'url': ('django.db.models.fields.CharField', [], {'max_length': '1000'})
+        },
+        u'taggit.tag': {
+            'Meta': {'object_name': 'Tag'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '100'})
+        },
+        u'taggit.taggeditem': {
+            'Meta': {'object_name': 'TaggedItem'},
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "u'taggit_taggeditem_tagged_items'", 'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'object_id': ('django.db.models.fields.IntegerField', [], {'db_index': 'True'}),
+            'tag': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "u'taggit_taggeditem_items'", 'to': u"orm['taggit.Tag']"})
         }
     }
 
